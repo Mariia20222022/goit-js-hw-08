e1220675fe9530db4a34536e753f9c7fac2e9eec
Mariia20222022 <113178594+Mariia20222022@users.noreply.github.com>
2023-02-16 10:58:12 +0000

    Deploying to gh-pages from @ Mariia20222022/goit-js-hw-08@63d81c67b0e287a88c94c5689e03504a2fabefbb 🚀

---
+++ 03-feedback.2460410e.js
@@ -0,0 +1,2 @@
+console.log("hello"),console.log("12345");
+//# sourceMappingURL=03-feedback.2460410e.js.map
---
+++ 03-feedback.7aebf300.js
@@ -0,0 +1,2 @@
+console.log("hello"),console.log("12345");
+//# sourceMappingURL=03-feedback.7aebf300.js.map
