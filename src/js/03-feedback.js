2d2ee1225a1b98a0212924bc61e4b5a84bed297e
Mariia20222022 <mariaorlova1818@gmail.com>
2023-02-17 11:47:33 +0200

    changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,32 +4,4 @@ const form = document.querySelector(`.feedback-form`);
 console.log(form);
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
-form.addEventListener(`input`, throttle(onInput, 500));
-form.addEventListener('input', throttle(populateTextarea, 500));
 const STORAGE_KEY = `feedback-form-state`;
-
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  event.currentTarget.reset();
-  // const objData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  // localStorage.removeItem(STORAGE_KEY);
-});
-function onInput(event) {
-  const fd = new FormData(form);
-  console.log(fd);
-  const obj = Object.fromEntries(fd);
-  console.log(obj);
-  const stringifiedObj = JSON.stringify(obj);
-  localStorage.setItem(STORAGE_KEY, stringifiedObj);
-}
-
-function populateTextarea() {
-  const getData = localStorage.getItem(STORAGE_KEY);
-  const parsedData = JSON.parse(getData);
-  if (parsedData) {
-    email.value = parsedData.email;
-    message.value = parsedData.message;
-  }
-
-  console.log(parsedData);
-}
