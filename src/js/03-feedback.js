535519e50121822f1d1a04ab66c5ce20d76298a4
Mariia20222022 <mariaorlova1818@gmail.com>
2023-02-18 14:38:07 +0200

    changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,29 +8,10 @@ const STORAGE_KEY = `feedback-form-state`;
 
 form.addEventListener('submit', throttle(validateForm), 500);
 
-function validateForm() {
-  const formDataObj = Object.fromEntries(myFormData.entries());
-  console.log(formDataObj);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formDataObj));
-}
 function validateForm(event) {
   event.preventDefault();
-  const myFormData = new FormData(event.target);
+  const data = new FormData(form);
 
-  // const formDataObj = Object.fromEntries(myFormData.entries());
-  // console.log(formDataObj);
-  // localStorage.setItem(STORAGE_KEY, JSON.stringify(formDataObj));
-  event.currentTarget.reset();
-  // const data = localStorage.getItem(STORAGE_KEY);
-  // const parsedData = JSON.parse(data);
-  // if (parsedData) {
-  //   console.log(parsedData);
-  // }
-}
-function getForm(event) {
-  const data = localStorage.getItem(STORAGE_KEY);
-  const parsedData = JSON.parse(data);
-  if (parsedData) {
-    console.log(parsedData);
-  }
+  const formDataObj = Object.fromEntries(myFormData.entries());
+  console.log(formDataObj);
 }
