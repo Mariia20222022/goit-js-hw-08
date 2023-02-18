6557b06ef3076e81d29d1603757e9ca2c647e021
Mariia20222022 <mariaorlova1818@gmail.com>
2023-02-18 15:51:17 +0200

    changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,33 @@
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
-// import throttle from 'lodash.throttle';
-const form = document.querySelector(`.feedback-form`);
-// console.log(form);
-console.log(form);
+const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
-const STORAGE_KEY = `feedback-form-state`;
-form.addEventListener(`submit`, submitForm);
+
+form.addEventListener('submit', submitForm);
+
 form.addEventListener('input', throttle(validateForm, 500));
-const data = {};
-function validateForm(event) {
-  event.preventDefault();
-  const data = new FormData(form);
+const data = {
+  email: '',
+  message: '',
+};
 
-  const formDataObj = Object.fromEntries(data.entries());
-
-  const stringifiedData = JSON.stringify(formDataObj);
-  console.log(stringifiedData);
-  localStorage.setItem(STORAGE_KEY, formDataObj);
-
-  // const receivedData = localStorage.getItem(STORAGE_KEY);
-  // if (receivedData) {
-  //   const parcedData = JSON.parse(receivedData);
-  //   console.log(parcedData);
-  // }
+getDataBase();
+function validateForm(e) {
+  const { name, value } = e.target;
+  data[name] = value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
-function submitForm(event) {
-  event.preventDefault();
-  event.currentTarget.reset();
+function submitForm(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
   console.log(data);
 }
 function getDataBase() {
-  const getData = localStorage.getItem(STORAGE_KEY);
+  const getData = localStorage.getItem('feedback-form-state');
   const parsedData = JSON.parse(getData);
   if (parsedData) {
-    // emailRef.value = parsedData.email;
-    // messageRef.value = parsedData.message;
-    console.log(parsedData);
+    email.value = parsedData.email;
+    message.value = parsedData.message;
   }
 }
